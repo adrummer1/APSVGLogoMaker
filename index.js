7d3a9c40d6ce91f3d116c218a27ee75922420bd6
adrummer1 <andrew@bluestemcreativeco.com>
2023-08-14 16:33:19 -0500

    Refactored index.js so it is pulling the shape parameters from shapes.js and troubleshoot shapes.js.test.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+const { Circle, Triangle, Square, UserShape } = require('./lib/shapes');
 
 async function generateLogo() {
     const userInput = await inquirer.prompt([
@@ -13,7 +13,7 @@ async function generateLogo() {
         {
             type: 'input',
             name: 'textColor',
-            messaage: 'Please enter a text color (keyword or hexadecimal number):',
+            message: 'Please enter a text color (keyword or hexadecimal number):',
         },
         {
             type: 'list',
@@ -33,35 +33,18 @@ async function generateLogo() {
     let logo;
     switch (userInput.shape) {
         case 'circle':
-            logo = new Circle(userInput.text, userInput.textColor, userInput.shapeColor);
-            break;
         case 'triangle':
-            logo = new Triangle(userInput.text, userInput.textColor, userInput.shapeColor);
-            break;
         case 'square':
-            logo = new Square(userInput.text, userInput.textColor, userInput.shapeColor);
+            logo = new UserShape(userInput);
             break;
         default:
             console.log('Invalid shape');
     }
 
     function generateShape(shape, shapeColor) {
-        let shapeContent = '';
-        switch (shape) {
-            case 'circle':
-                shapeContent = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
-                break;
-            case 'triangle':
-                shapeContent = `<polygon points="150,20 50,180 250,180" fill="${shapeColor}" />`;
-                break;
-            case 'square':
-                shapeContent = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
-                break;
-            default:
-                console.log('Invalid shape');
-        }
-        return shapeContent;
-    }    
+        const userShape = new UserShape({ shape, shapeColor, text: userInput.text, textColor: userInput.textColor });
+        return userShape.draw();
+    } 
 
     const svgContent = `<svg width="300" height="200">
         <text x="50%" y="50%" fill="${userInput.textColor}" text-anchor="middle">${userInput.text}</text>
