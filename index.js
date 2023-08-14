0bf66ff6481f70e6677b5ab8a540f4d0ce7c4743
adrummer1 <andrew@bluestemcreativeco.com>
2023-08-14 11:19:16 -0500

    Defined shape and created function for generateShape.

--- index.js
+++ index.js
@@ -28,24 +28,44 @@ async function generateLogo() {
         },
     ])
 
+    const shape = userInput.shape;
+
     let logo;
     switch (shape) {
         case 'circle':
-            logo = new Circle(text, textColor, shapeColor);
+            logo = new Circle(userInput.text, userInput.textColor, userInput.shapeColor);
             break;
         case 'triangle':
-            logo = new Triangle(text, textColor, shapeColor);
+            logo = new Triangle(userInput.text, userInput.textColor, userInput.shapeColor);
             break;
         case 'square':
-            logo = new Square(text, textColor, shapeColor);
+            logo = new Square(userInput.text, userInput.textColor, userInput.shapeColor);
             break;
         default:
             console.log('Invalid shape');
     }
 
+    function generateShape(shape, shapeColor) {
+        let shapeContent = '';
+        switch (shape) {
+            case 'circle':
+                shapeContent = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+                break;
+            case 'triangle':
+                shapeContent = `<polygon points="150,20 50,180 250,180" fill="${shapeColor}" />`;
+                break;
+            case 'square':
+                shapeContent = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
+                break;
+            default:
+                console.log('Invalid shape');
+        }
+        return shapeContent;
+    }    
+
     const svgContent = `<svg width="300" height="200">
-    <text x="50%" y="50%" fill="${textColor}" text-anchor="middle">${text}</text>
-    ${generateShape(shape, shapeColor)}
+    <text x="50%" y="50%" fill="${userInput.textColor}" text-anchor="middle">${userInput.text}</text>
+    ${generateShape(userInput.shape, userInput.shapeColor)}
   </svg>`;
 
     fs.writeFileSync('logo.svg', svgContent);
